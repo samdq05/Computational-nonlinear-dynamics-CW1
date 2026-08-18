b92025a1c7f227aa05d79f98354bbb0a2969abf7
samdq05 <146759948+samdq05@users.noreply.github.com>
2026-08-18 08:33:08 +0100

    Delete Task3.1TestScript.jl
    redundant

--- Task3.1TestScript.jl
+++
@@ -1,6 +0,0 @@
-# initialise
-prange = vcat(-1.1:0.01:-0.8, -0.15:0.01:0.05)
-values = 21
-vg = range(-2.5, 2.5; length = values)
-wg = range(-2.5, 2.5; length = values)
-ics = [[v, w] for w in wg for v in vg]
